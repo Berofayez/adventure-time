359df4a48ccbc730a9224ef6c7bde2df676d0670
Abeer Garges <agfayez2018@gmail.com>
2026-04-22 11:15:32 -0700

    initial commit

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -1,9 +1,34 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class AdventureTime {
     static void main() {
+        loadAdventure();
 
+    }
+
+    public static void loadAdventure()
+    {
+        try
+        {
+            FileReader fileReader = new FileReader("adventure1.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while (line != null){
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+            fileReader.close();
+
+        }
+        catch (Exception e){
+
+        }
     }
 }
